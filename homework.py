c360d5bd46022cdc27e19c4a150a89e8ee00dbbc
Alexey <loskuta42@yandex.ru>
2021-04-17 10:50:03 +0300

    after_review2_v1

--- homework.py
+++ homework.py
@@ -12,11 +12,11 @@ load_dotenv()
 PRAKTIKUM_TOKEN = os.getenv('PRAKTIKUM_TOKEN')
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
 CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
+URL = 'https://praktikum.yandex.ru/api/user_api/homework_statuses/'
 STATUSES = {
     'approved': ('Ревьюеру всё понравилось, '
                  'можно приступать к следующему уроку.'),
-    'rejected': 'К сожалению в работе нашлись ошибки.',
-    'reviewing': 'Работа взята в ревью.'
+    'rejected': 'К сожалению в работе нашлись ошибки.'
 }
 
 
@@ -24,26 +24,23 @@ def parse_homework_status(homework):
     homework_name = homework.get('homework_name')
     if homework.get('status') in STATUSES:
         key = homework.get('status')
-        verdict = STATUSES.get(key)
+        verdict = STATUSES[key]
         return f'У вас проверили работу "{homework_name}"!\n\n{verdict}'
-    else:
-        logging.error('Неизвестный статус работы')
-        return 'Неизвестный статус работы'
+    logging.error('Неизвестный статус работы')
+    return 'Неизвестный статус работы'
 
 
 def get_homework_statuses(current_timestamp):
     from_date = current_timestamp
-    url = 'https://praktikum.yandex.ru/api/user_api/homework_statuses/'
     try:
         homework_statuses = requests.get(
-            url,
+            URL,
             params={'from_date': from_date},
             headers={'Authorization': f'OAuth {PRAKTIKUM_TOKEN}'}
         )
         return homework_statuses.json()
     except requests.exceptions.RequestException as e:
-        logging.error(f'Ошибка запроса страницы {url}')
-        raise SystemExit(e)
+        logging.error(f'Ошибка запроса({e}) страницы {URL}')
 
 
 def send_message(message, bot_client):
@@ -58,8 +55,8 @@ def main():
         try:
             new_homework = get_homework_statuses(current_timestamp)
             if new_homework.get('homeworks'):
-                if new_homework.get('homeworks') is not None and\
-                        new_homework.get('homeworks')[0]:
+                homeworks = new_homework.get('homeworks')
+                if homeworks and len(homeworks) > 0:
                     send_message(parse_homework_status(
                         new_homework.get('homeworks')[0]),
                         bot
